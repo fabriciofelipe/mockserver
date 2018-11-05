0438836800d60906978435a1cf76a039d26d3601
fabriciofelipe <fabriciooak@gmail.com>
2018-11-05 15:01:10 -0200

    created exemplos the requisitions POST GET

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mockserver
+
+Para rodar bastar rodar o run.sh, que é resposnável em fazer o build de uma nova imagem e subir o container
--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ CONTAINER_NAME='mock'
 #sudo docker run -d -v $PWD:/home/wiremock -p 8043:8443 --name https_$CONTAINER_NAME rodolpheche/wiremock --https-port 8443 --verbose
 #sudo docker ps | grep $CONTAINER_NAME
 
-sudo docker run --name CONTAINER_NAME -d -p 8070:8080 mockserver
+docker build -t mockserver .
+sudo docker run --name $CONTAINER_NAME -d -p 8070:8080 mockserver
