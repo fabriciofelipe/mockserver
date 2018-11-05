03ce2a2751b50dbf7abc65eb14f9651cf0d5ac6b
fabriciofelipe <fabriciooak@gmail.com>
2018-11-05 15:45:54 -0200

    update file clear

--- clear.sh
+++ clear.sh
@@ -1,4 +1,4 @@
-sudo docker rm -fv mockserver || echo 'MockServer não estava em execução'
-sudo docker rm -fv https_mockserver || echo 'https_MockServer não estava em execução'
-sudo docker ps | grep mockserver
-sudo docker ps | grep https_mockserver
+sudo docker rm -fv mock || echo 'MockServer não estava em execução'
+#sudo docker rm -fv https_mockserver || echo 'https_MockServer não estava em execução'
+sudo docker ps | grep mock
+sudo docker ps | grep mock
